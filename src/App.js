6cacd9b33993b252e127b3f11241e5dee9266ad9
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-04-17 17:56:41 +0600

    how to set data dynamicly

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const style= {
     color:"red"
   }
+  const works =["shalik","monai", "kapor dhoua","raing paila maja"]
+
   return (
     <div className="App">
       {/* // how to apply style from variable */}
@@ -12,15 +14,16 @@ function App() {
       <h1 className="newbie" style={style}> my first heading with variable style</h1>
       {/* how to apply style inline */}
       <h2 style={{backgroundColor:"red"}} >My second heading this is inline style</h2>
-    <Person></Person>
-    <Person></Person>
+    <Person name="Mr. xenon" job="hosting"></Person>
+    <Person name = "Mr.solimoddui" job="wash" ></Person>
+    <Person name="shakil" job={works[3]}></Person>
     </div>
   );
 }
-function Person(){
-return <div style={{border:"1px solid red" ,margin:"2px"}} >
-  <h1 >Name: Sakib al ahsan</h1>
-  <h2>job: lafalafi kora</h2>
+function Person(props){
+return <div style={{border:"1px solid red" ,margin:"10px"}} >
+  <h1 >Name:{props.name}</h1>
+  <h2>job: {props.job}</h2>
 </div>
 }
 
