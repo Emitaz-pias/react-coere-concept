54ad4da8f9ea41d8cc8523309057c746cd04e6e2
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-04-17 17:31:29 +0600

    how to use component

--- src/App.js
+++ src/App.js
@@ -12,8 +12,16 @@ function App() {
       <h1 className="newbie" style={style}> my first heading with variable style</h1>
       {/* how to apply style inline */}
       <h2 style={{backgroundColor:"red"}} >My second heading this is inline style</h2>
+    <Person></Person>
+    <Person></Person>
     </div>
   );
+}
+function Person(){
+return <div style={{border:"1px solid red" ,margin:"2px"}} >
+  <h1 >Name: Sakib al ahsan</h1>
+  <h2>job: lafalafi kora</h2>
+</div>
 }
 
 export default App;
