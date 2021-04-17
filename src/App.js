ae6aee64628f698af50bb7abd291d9bdc1db8e37
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-04-17 17:17:47 +0600

    how to apply style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const style= {
+    color:"red"
+  }
   return (
     <div className="App">
+      {/* // how to apply style from variable */}
       
+      <h1 className="newbie" style={style}> my first heading with variable style</h1>
+      {/* how to apply style inline */}
+      <h2 style={{backgroundColor:"red"}} >My second heading this is inline style</h2>
     </div>
   );
 }
