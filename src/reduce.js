e35b76fda1fdbddb9458b86e68d9f368b593694a
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-05-06 13:57:19 +0600

    reducer done

--- src/reduce.js
+++ src/reduce.js
@@ -1,1 +1,7 @@
-
+const nums =[12,324,56,78,8]
+ 
+const total = nums.reduce((sum,num)=>sum+num,0)
+console.log(total);
+const friends = [{name: "Rashed",money: 414},{name: "Rashed",money: 414},{name: "kashed",money: 44},{name: "pashed",money: 41}]
+const totalMoney = friends.reduce((sum,friends)=>sum+friends.money,0)
+console.log(totalMoney);
